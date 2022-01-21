f0dc2de200a687ccb13640bbec69b2ad6d713ff5
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-21 16:26:29 -0300

    create post routes fror list and create posts

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -1,7 +1,25 @@
 defmodule BlogWeb.PostController do
   use BlogWeb, :controller
 
-  def index(conn, params) do
+  alias Blog.Posts.Post
 
+  def index(conn, _params) do
+    posts = Blog.Repo.all(Post)
+    render(conn, "index.html", posts: posts)
+  end
+
+  def new(conn, params) do
+    changeset = Post.changeset(%Post{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def show(conn, %{"id" => id}) do
+    post = Blog.Repo.get!(Post, id)
+    render(conn, "show.html", post: post)
+  end
+
+  def create(conn, %{"id" => id}) do
+    post = Blog.Repo.get!(Post, id)
+    render(conn, "show.html", post: post)
   end
 end
--- lib/blog_web/router.ex
+++ lib/blog_web/router.ex
@@ -20,7 +20,13 @@ defmodule BlogWeb.Router do
   scope "/", BlogWeb do
     pipe_through :browser
 
+    get "/posts/new", PostController, :new
     get "/posts", PostController, :index
+    get "/posts/:id", PostController, :show
+
+    post "/posts", PostController, :create
+
+
     get "/", PageController, :index
   end
 
