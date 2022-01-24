dec09c57c71c338d86ea88cc851821d62ba3f929
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-24 17:43:15 -0300

    create edit update and delete controllers

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -13,6 +13,12 @@ defmodule BlogWeb.PostController do
     render(conn, "new.html", changeset: changeset)
   end
 
+  def edit(conn, %{"id" => id}) do
+    post = Blog.Repo.get(Blog.Posts.Post, id)
+    changeset = Post.changeset(post)
+    render(conn, "edit.html", post: post, changeset: changeset)
+  end
+
   def show(conn, %{"id" => id}) do
     post = Blog.Repo.get!(Post, id)
     render(conn, "show.html", post: post)
@@ -26,6 +32,30 @@ defmodule BlogWeb.PostController do
       {:ok, post} ->
         conn
         |> put_flash(:info, "Post successfully created!")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    post = Blog.Repo.get!(Blog.Posts.Post, id)
+    Blog.Repo.delete!(post)
+
+    conn
+      |> put_flash(:info, "Post Successfully deleted.")
+      |> redirect(to: Routes.post_path(conn, :index))
+  end
+
+  def update(conn, %{"id" => id, "post" => post_params}) do
+    post = Blog.Repo.get!(Blog.Posts.Post, id)
+    changeset = Blog.Posts.Post.changeset(post, post_params)
+    post = Blog.Repo.update(changeset)
+
+    case post do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post successfully updated!")
         |> redirect(to: Routes.post_path(conn, :show, post))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
