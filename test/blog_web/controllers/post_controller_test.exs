5d714f318c96ec5fff9fb0a2735957510e25808d
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-25 12:55:27 -0300

    get post by id controller_test

--- test/blog_web/controllers/post_controller_test.exs
+++ test/blog_web/controllers/post_controller_test.exs
@@ -10,9 +10,15 @@ defmodule BlogWeb.PostControllerTest do
     description: "desc_test"
   }
 
-  test "LIst all posts", %{conn: conn} do
+  test "List all posts", %{conn: conn} do
     Blog.Posts.create_post(@valid_post)
     conn = get(conn, Routes.post_path(conn, :index))
+    assert html_response(conn, 200) =~ "teste"
+  end
+
+  test "Get post by id", %{conn: conn} do
+    {:ok, post} = Blog.Posts.create_post(@valid_post)
+    conn = get(conn, Routes.post_path(conn, :show, post))
     assert html_response(conn, 200) =~ "teste"
   end
 end
