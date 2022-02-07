aba32a1eea4d7a2f0c679d797f241072d3666698
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-07 15:50:22 -0300

    test should create an comment

--- test/blog_web/channels/comments_channel_test.exs
+++ test/blog_web/channels/comments_channel_test.exs
@@ -12,15 +12,31 @@ defmodule BlogWeb.CommentsChannelTest do
     description: "test_desc"
   }
 
-  test "should connect to socket" do
-
+  setup do
     {:ok, post} = Posts.create_post(@valid_post)
     {:ok, socket} = connect(UserSocket, %{})
 
+    {:ok, socket: socket, post: post}
+  end
+
+  test "should connect to socket", %{socket: socket, post: post} do
     {:ok, comments, socket} = subscribe_and_join(socket, "comments:#{post.id}", %{})
 
-   assert post.id == socket.assigns.post_id
-   assert [] == comments.comments
+    assert post.id == socket.assigns.post_id
+    assert [] == comments.comments
+  end
+
+  test "should create an comment", %{socket: socket, post: post} do
+    {:ok, _comments, socket} = subscribe_and_join(socket, "comments:#{post.id}", %{})
+
+    ref = push(socket, "comment:add", %{"content" => "content_test"})
+
+    msg = %{comment: %{content: "content_test"}}
+    assert_reply ref, :ok, %{}
+    broadcast_event = "comments:#{post.id}:new"
+
+    assert_broadcast ^broadcast_event, msg
+    refute is_nil(msg)
   end
 
 end
