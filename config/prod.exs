8a7940208bf398e19e5ea26dfd57810718c014ab
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-07 16:23:20 -0300

    host app

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :logger, level: :info
 # to the previous section and set your `:url` port to 443:
 #
 config :blog, BlogWeb.Endpoint,
-  url: [host: "https://elixir-blog.gigalixirapp.com/", port: 443],
+  url: [host: "elixir-blog.gigalixirapp.com/", port: 443],
   https: [
     port: 443,
     cipher_suite: :strong,
