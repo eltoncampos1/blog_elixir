7b539f56e8872bfaa2b7df07f06813ed994aaa8c
eltoncampos1 <eltoncampos36@gmail.com>
2022-02-11 13:39:30 -0300

    format

--- test/blog_web/controllers/auth_controller_test.exs
+++ test/blog_web/controllers/auth_controller_test.exs
@@ -11,7 +11,7 @@ defmodule BlogWeb.AuthControllerTest do
     info: %{
       email: "auth.info@email",
       first_name: "auth.info.first_name",
-      image: "auth.info.image",
+      image: "auth.info.image"
     },
     provider: "google"
   }
@@ -23,36 +23,39 @@ defmodule BlogWeb.AuthControllerTest do
     info: %{
       email: "adsd@asd",
       first_name: nil,
-      image: nil,
+      image: nil
     },
     provider: "google"
   }
 
   test "callback success", %{conn: conn} do
     conn =
-    conn
-    |>assign(:ueberauth_auth, @ueberauth)
-    |>get(Routes.auth_path(conn, :callback, "google"))
+      conn
+      |> assign(:ueberauth_auth, @ueberauth)
+      |> get(Routes.auth_path(conn, :callback, "google"))
+
     assert redirected_to(conn) == Routes.page_path(conn, :index)
     conn = get(conn, Routes.page_path(conn, :index))
-    assert html_response(conn, 200) =~  "Welcome!!! #{@ueberauth.info.email} "
+    assert html_response(conn, 200) =~ "Welcome!!! #{@ueberauth.info.email} "
   end
 
   test "callback error", %{conn: conn} do
     conn =
-    conn
-    |>assign(:ueberauth_auth, @ueberauth_error)
-    |>get(Routes.auth_path(conn, :callback, "google"))
+      conn
+      |> assign(:ueberauth_auth, @ueberauth_error)
+      |> get(Routes.auth_path(conn, :callback, "google"))
+
     assert redirected_to(conn) == Routes.page_path(conn, :index)
     conn = get(conn, Routes.page_path(conn, :index))
-    assert html_response(conn, 200) =~  "Something wrong"
+    assert html_response(conn, 200) =~ "Something wrong"
   end
 
   test "logout", %{conn: conn} do
     conn =
-    conn
-    |>Plug.Test.init_test_session(user_id: 1)
-    |>get(Routes.auth_path(conn, :logout))
+      conn
+      |> Plug.Test.init_test_session(user_id: 1)
+      |> get(Routes.auth_path(conn, :logout))
+
     assert redirected_to(conn) == Routes.page_path(conn, :index)
   end
 end
--- test/blog_web/controllers/post_controller_test.exs
+++ test/blog_web/controllers/post_controller_test.exs
@@ -40,16 +40,18 @@ defmodule BlogWeb.PostControllerTest do
 
   test "Render form to create a new post", %{conn: conn} do
     conn =
-    conn
-    |>Plug.Test.init_test_session(user_id: 1)
-    |> get(Routes.post_path(conn, :new))
+      conn
+      |> Plug.Test.init_test_session(user_id: 1)
+      |> get(Routes.post_path(conn, :new))
+
     assert html_response(conn, 200) =~ "Title"
   end
 
   test "Render form to create a new post with no user auth", %{conn: conn} do
     conn =
-    conn
-    |> get(Routes.post_path(conn, :new))
+      conn
+      |> get(Routes.post_path(conn, :new))
+
     assert redirected_to(conn) == Routes.page_path(conn, :index)
 
     conn = get(conn, Routes.page_path(conn, :index))
@@ -58,9 +60,9 @@ defmodule BlogWeb.PostControllerTest do
 
   test "create a new post", %{conn: conn} do
     conn =
-    conn
-    |> Plug.Test.init_test_session(user_id: 1)
-    |> post(Routes.post_path(conn, :create), post: @valid_post)
+      conn
+      |> Plug.Test.init_test_session(user_id: 1)
+      |> post(Routes.post_path(conn, :create), post: @valid_post)
 
     assert %{id: id} = redirected_params(conn)
     assert redirected_to(conn) == Routes.post_path(conn, :show, id)
@@ -71,9 +73,9 @@ defmodule BlogWeb.PostControllerTest do
 
   test "create a new post with invalid params", %{conn: conn} do
     conn =
-    conn
-    |> Plug.Test.init_test_session(user_id: 1)
-    |>post(Routes.post_path(conn, :create), post: %{})
+      conn
+      |> Plug.Test.init_test_session(user_id: 1)
+      |> post(Routes.post_path(conn, :create), post: %{})
 
     assert html_response(conn, 200) =~ "can&#39;t be blank"
   end
@@ -84,10 +86,11 @@ defmodule BlogWeb.PostControllerTest do
     test "Render form to edit an post", %{conn: conn} do
       user = Blog.Accounts.get_user!(1)
       {:ok, post} = Blog.Posts.create_post(user, @valid_post)
+
       conn =
-      conn
-      |> Plug.Test.init_test_session(user_id: 1)
-      |> get(Routes.post_path(conn, :edit, post))
+        conn
+        |> Plug.Test.init_test_session(user_id: 1)
+        |> get(Routes.post_path(conn, :edit, post))
 
       assert html_response(conn, 200) =~ "a"
     end
@@ -95,10 +98,11 @@ defmodule BlogWeb.PostControllerTest do
     test "Render form to edit an post with no auth", %{conn: conn} do
       user = Blog.Accounts.get_user!(1)
       {:ok, post} = Blog.Posts.create_post(user, @valid_post)
+
       conn =
-      conn
-      |> Plug.Test.init_test_session(user_id: 2)
-      |> get(Routes.post_path(conn, :edit, post))
+        conn
+        |> Plug.Test.init_test_session(user_id: 2)
+        |> get(Routes.post_path(conn, :edit, post))
 
       assert redirected_to(conn) == Routes.page_path(conn, :index)
       conn = get(conn, Routes.page_path(conn, :index))
@@ -107,30 +111,31 @@ defmodule BlogWeb.PostControllerTest do
 
     test "edit an post", %{conn: conn, post: post} do
       conn =
-      conn
-      |> Plug.Test.init_test_session(user_id: 1)
-      |>put(Routes.post_path(conn, :update, post), post: @updated_post)
+        conn
+        |> Plug.Test.init_test_session(user_id: 1)
+        |> put(Routes.post_path(conn, :update, post), post: @updated_post)
+
       assert %{id: id} = redirected_params(conn)
       assert redirected_to(conn) == Routes.post_path(conn, :show, id)
 
-      conn = get(conn,Routes.post_path(conn, :show, id))
+      conn = get(conn, Routes.post_path(conn, :show, id))
       assert html_response(conn, 200) =~ "desc_test_updated"
     end
 
     test "edit an post with invalid params", %{conn: conn, post: post} do
       conn =
-      conn
-      |> Plug.Test.init_test_session(user_id: 1)
-      |>put(Routes.post_path(conn, :update, post), post: %{title: nil, description: nil})
+        conn
+        |> Plug.Test.init_test_session(user_id: 1)
+        |> put(Routes.post_path(conn, :update, post), post: %{title: nil, description: nil})
 
       assert html_response(conn, 200) =~ "can&#39;t be blank"
     end
 
     test "delete post", %{conn: conn, post: post} do
       conn =
-      conn
-      |> Plug.Test.init_test_session(user_id: 1)
-      |>delete(Routes.post_path(conn, :delete, post))
+        conn
+        |> Plug.Test.init_test_session(user_id: 1)
+        |> delete(Routes.post_path(conn, :delete, post))
 
       assert redirected_to(conn) == Routes.post_path(conn, :index)
       assert_error_sent 404, fn -> get(conn, Routes.post_path(conn, :show, post)) end
